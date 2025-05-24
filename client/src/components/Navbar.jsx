d01795e335c8700ca1fad2caf2ce10c8dc80e21b
Sarangi Nimesha <srnginimesha@gmail.com>
2025-05-24 21:34:50 +0530

    fine tune

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
    <nav className="bg-white shadow-md p-4 sticky top-0 z-50">
   <div className="max-w-screen-xl mx-auto flex justify-between items-center">
-    <h1 className="text-2xl font-bold text-blue-900">Globle One Company</h1>
+    <h1 className="text-2xl font-bold text-blue-900">Globle One Campus</h1>
     <div className="space-x-6 text-blue-700">
      <Link to="/" className="hover:text-blue-600">Home</Link>
           <Link to="/about" className="hover:text-blue-600">About</Link>
